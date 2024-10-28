55ff61000eb4aeb2fd2db7c3cf6f39557890ce84
Ashlyn Carter <eannacarter@gmail.com>
2024-10-28 10:20:57 -0400

    Commented JS file

--- js/index.js
+++ js/index.js
@@ -5,15 +5,17 @@ changeColor = () => {
     const text = document.getElementById('the-text');
     console.log("Clicked");
 
+    //If blue selected, make title blue
     if(blue.checked) {
         text.className = 'blue';
         console.log("blue");
     }
+    //If purple selected, make title purple
     else if (purple.checked) {
         text.className = 'purple';
         console.log("purple");
     }
-
+    //If nothing selected, make title pink
     else {
         text.className = 'default';
         console.log("default");
