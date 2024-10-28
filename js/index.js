575448614e14750d58e7e8354b708fe4b4c53409
Ashlyn Carter <eannacarter@gmail.com>
2024-10-28 10:25:59 -0400

    Added more color options

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ console.log("Connected");
 changeColor = () => {
     const blue = document.getElementById('blue');
     const purple = document.getElementById('purple');
+    const pink = document.getElementById('pink');
+    const cyan = document.getElementById('cyan');
     const text = document.getElementById('the-text');
     console.log("Clicked");
 
@@ -14,6 +16,16 @@ changeColor = () => {
     else if (purple.checked) {
         text.className = 'purple';
         console.log("purple");
+    }
+    //If pink selected, make title pink
+    else if (pink.checked) {
+        text.className = 'pink';
+        console.log("pink");
+    }
+    //If cyan selected, make title cyan
+    else if (cyan.checked) {
+        text.className = 'cyan';
+        console.log("cyan");
     }
     //If nothing selected, make title pink
     else {
